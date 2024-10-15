7e3c2be3765fbc39394a5256166fdac7b31ecbc6
406-08\student <fca>
2024-10-15 12:05:43 +0900

    サムネイル作り　データ復旧　スクリプトエラー

---
+++ Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameConto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public static GameConto Instance{get; private set; }
+    public int score { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    //スタートメソッド
+    public void StartGame()
+    {
+        //SceneData.score = 0;
+        SceneManager.LoadScene("GameScenes");
+    }
+
+    //エンドメソッド
+    public void EndGame()
+    {
+        //獲得したスコアとリザルト画面へ遷移
+        //SceneData.score = ScoreScript.instance.GetCurrentScore();
+
+        SceneManager.LoadScene("ResultScenes");
+    }
+}
---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int count = 0;
+    void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+        GetComponent<ParticleSystem>().Play(); 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
+        count++;
+        if(count>1000)
+        {
+            ///1000fで消去
+            Destroy(this.gameObject);
+            count = 0;
+        }
+    }
+}
