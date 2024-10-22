5fb597828dc6f385040f67f6f139eb12ffe58520
Koutexya <fumitan0129@gmail.com>
2024-10-22 12:18:13 +0900

    no message

--- Assets/Scenes/object/start/CameraContololl.cs
+++ Assets/Scenes/object/start/CameraContololl.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CameraContololl : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 中心点
+    [SerializeField] private Vector3 _center = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    // 回転軸
+    [SerializeField] private Vector3 _axis = Vector3.up;
+
+    // 円運動周期
+    [SerializeField] private float _period = 2;
+
+    private void Update()
     {
-        
+        // 中心点centerの周りを、軸axisで、period周期で円運動
+        transform.RotateAround(
+            _center,
+            _axis,
+            360 / _period * Time.deltaTime
+        );
     }
 }
